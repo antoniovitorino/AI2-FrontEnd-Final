74b4516eb3cbac0d9c2c155985d9b8ceebce6421
António Vitorino <pv21068@alunos.estgv.ipv.pt>
2023-06-15 19:30:43 +0100

    auth2

--- src/auth-service.js
+++ src/auth-service.js
@@ -1,19 +1,20 @@
-class AuthService { 
-    login(email, password) {
-      return axios
-        .post("https://jogatanas-api.onrender.com/users/login", { email, password }) 
-        .then(res => {
-          if (res.data.token) {
-            sessionStorage.setItem("user", JSON.stringify(res.data));
-          }
-          return res.data;
-        }, reason => { throw new Error('Utilizador Inválido'); });
-    }
-    logout() { sessionStorage.removeItem("user"); }
-    getCurrentUser() { 
-      return JSON.parse(sessionStorage.getItem('user'));
-    }
-  } 
-  
-  export default new AuthService();
-  
+import axios from 'axios';
+
+class AuthService {
+  login(email, password) {
+    return axios
+      .post("https://jogatanas-api.onrender.com/users/login", { email, password })
+      .then(res => {
+        if (res.data.token) {
+          sessionStorage.setItem("user", JSON.stringify(res.data));
+        }
+        return res.data;
+      }, reason => { throw new Error('Utilizador Inválido'); });
+  }
+  logout() { sessionStorage.removeItem("user"); }
+  getCurrentUser() {
+    return JSON.parse(sessionStorage.getItem('user'));
+  }
+}
+
+export default new AuthService();
