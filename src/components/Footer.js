88af61f38312bcf7cce8477cfcf39937f66de9c0
António Vitorino <pv21068@alunos.estgv.ipv.pt>
2023-06-15 04:50:19 +0100

    correção nome

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
 
 <div className="container-fluid px-5 py-3 ">
   <div> 
-          <p><span className="text-light">2023 Joganas - Trabalho de: António Vitorino (21068) & Inês (20000 ?)  </span></p>
+          <p><span className="text-light">2023 Joganas - Trabalho de: António Vitorino (21068) & Inês Mateus (17945)  </span></p>
           <p><span className="text-light">IPV | ESTGV | TDM | AI2 | Professores: Frederico Fonseca, Steven Abrantes e Tiago Rebelo</span></p>
           </div>
           <div>
