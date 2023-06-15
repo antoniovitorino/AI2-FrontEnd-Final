01a3753461eb83f6f20caed32141427f462ad989
António Vitorino <pv21068@alunos.estgv.ipv.pt>
2023-06-15 22:31:36 +0100

    400-2

--- src/views/404/index.js
+++ src/views/404/index.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
-import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
+import Navbar from '../../components/Navbar';
+import Footer from '../../components/Footer';
 
 const NotFoundPage = () => {
   useEffect(() => {
