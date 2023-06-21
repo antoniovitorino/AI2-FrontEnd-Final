b2ac4f825ddc1df3c5ccfbf010596e37212ee045
António Vitorino <pv21068@alunos.estgv.ipv.pt>
2023-06-21 20:25:34 +0100

    navbar

--- src/components/NavBarOutras.js
+++ src/components/NavBarOutras.js
@@ -4,10 +4,10 @@ function NavBarOutras() {
   // Conteúdo do navbar para as outras páginas
   return (
     <nav className="navbar">
-      <div className="container-fluid my-3 mx-5">
+      <div className="container-fluid px-5">
         <div>
         <a className="navbar-brand" href="/">
-          <img src="/imagens/logo.png" width="200px" className="d-inline-block align-top" alt="Logo MovieRise" />
+          <img src="/imagens/logo.png" width="150px" className="d-inline-block align-top" alt="Logo MovieRise" />
         </a>
         </div>
       </div>
