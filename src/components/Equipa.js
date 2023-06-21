ae136f769c7ef77c8d8558c0dca4a3196cde5257
António Vitorino <pv21068@alunos.estgv.ipv.pt>
2023-06-21 21:24:25 +0100

    equipa-tilt2

--- src/components/Equipa.js
+++ src/components/Equipa.js
@@ -53,9 +53,10 @@ const Equipas = () => {
     <div className="container-fluid px-5 py-3 sobre marginFooter">
       <div className="text-start text-light mb-5"><h1 >Equipa Jogatanas</h1></div>
         <div className="row">
-        <Tilt options={defaultOptions}>
+        
           {equipas.slice(0, 12).map(equipa => (
           <div className="col-lg-2 col-md-6 col-sm-6 col-xs-12 col-mb-5" key={equipa.id}>
+            <Tilt options={defaultOptions}>
             <div className="card ">
               <img className="card-img-top" src={`https://jogatanas-api.onrender.com/midia/${equipa.fotoId}`} alt={equipa.nome} />  
               <div className="card-body text-bg-dark">
@@ -65,9 +66,10 @@ const Equipas = () => {
                 <p>{equipa.biografia}</p>
               </div>
             </div>
+            </Tilt>
           </div>
         ))}
-        </Tilt>
+        
       </div>
     </div>
 
